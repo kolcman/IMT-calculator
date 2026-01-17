751390115f4504a36ed375be26b649208f5780b6
Kolcman <m.kolcman@yandex.ru>
2026-01-17 22:46:50 +0300

    add base logic

--- main.go
+++ main.go
@@ -6,8 +6,13 @@ import (
 )
 
 func main() {
+	fmt.Println("******* Калькулято ИМТ *******")
 	const IMT_POWER = 2
-	height, weight := 1.8, 100.0
+	var height, weight float64
+	fmt.Print("Введите свой рост в метрах: ")
+	fmt.Scan(&height)
+	fmt.Print("Введите свой вес: ")
+	fmt.Scan(&weight)
 	IMT := weight / math.Pow(height, IMT_POWER)
-	fmt.Printf("Yout IMT: %f", IMT)
+	fmt.Printf("Ваш ИМТ: %f", IMT)
 }
